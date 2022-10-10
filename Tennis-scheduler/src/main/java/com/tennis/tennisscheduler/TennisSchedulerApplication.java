0a61edc201f1c8f86b809b83edb174dcaaca9f84
nevenaatic <74498373+nevenaatic@users.noreply.github.com>
2022-10-10 15:38:09 +0200

    feat: project initialization

---
+++ Tennis-scheduler/src/main/java/com/tennis/tennisscheduler/TennisSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.tennis.tennisscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisSchedulerApplication.class, args);
+	}
+
+}
