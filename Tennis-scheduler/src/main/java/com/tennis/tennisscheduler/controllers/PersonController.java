da471eef9aeac1ac59c835358de31e3f21326578
Danilo Rapaic <rapaic.danilo9@gmail.com>
2022-10-11 12:16:32 +0200

    bug-fixed: added RestController

--- Tennis-scheduler/src/main/java/com/tennis/tennisscheduler/controllers/PersonController.java
+++ Tennis-scheduler/src/main/java/com/tennis/tennisscheduler/controllers/PersonController.java
@@ -3,14 +3,18 @@ package com.tennis.tennisscheduler.controllers;
 import com.tennis.tennisscheduler.services.PersonService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/person")
 public class PersonController {
 
     private PersonService personService;
 
-    @RequestMapping("/")
+
+    @PostMapping
     public String display()
     {
         return "index";
