d1e76904dd52dd277afcfe3d928874499cf91682
Danilo Rapaic <rapaic.danilo9@gmail.com>
2022-10-12 14:45:07 +0200

    feat: Adding pom dependency and xml files

--- Tennis-scheduler/src/main/java/com/tennis/tennisscheduler/controllers/PersonController.java
+++ Tennis-scheduler/src/main/java/com/tennis/tennisscheduler/controllers/PersonController.java
@@ -5,11 +5,9 @@ import com.tennis.tennisscheduler.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/person")
