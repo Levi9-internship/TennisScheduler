d6f88b5af013faca1055682970a55f5db31f4153
nevenaatic <74498373+nevenaatic@users.noreply.github.com>
2022-10-13 16:10:06 +0200

    Update StartupSwagger.java
    Update StartupSwagger.java

--- Tennis-scheduler/src/main/java/com/tennis/tennisscheduler/config/StartupSwagger.java
+++ Tennis-scheduler/src/main/java/com/tennis/tennisscheduler/config/StartupSwagger.java
@@ -1,5 +1,7 @@
 package com.tennis.tennisscheduler.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -17,7 +19,8 @@ public class StartupSwagger {
 
         try{
             desktop.browse(new URI("http://localhost:8081/swagger-ui/index.html#/"));
-        }catch(Exception e){}
-
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
