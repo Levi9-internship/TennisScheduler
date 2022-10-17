4da1f77421f44e81648fdb6b4da6f62460acedb9
nevenaatic <74498373+nevenaatic@users.noreply.github.com>
2022-10-17 10:45:26 +0200

    feat: check date, working hours and timeslot duration

--- Tennis-scheduler/src/main/java/com/tennis/tennisscheduler/services/TimeslotService.java
+++ Tennis-scheduler/src/main/java/com/tennis/tennisscheduler/services/TimeslotService.java
@@ -2,9 +2,14 @@ package com.tennis.tennisscheduler.services;
 
 import com.tennis.tennisscheduler.models.Timeslot;
 import com.tennis.tennisscheduler.repositories.TimeslotRepository;
+import org.apache.commons.lang3.time.DateUtils;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+import org.joda.time.Minutes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -41,5 +46,57 @@ public class TimeslotService {
         //set tennis court
         timeslotNew.setPerson(personService.findById(personId));
         return timeslotRepository.save(timeslotNew);
+    }
+
+    public String checkTimeslotValidity(Timeslot timeslot) {
+        if(checkValidationDate(timeslot)){
+            if(checkDate(timeslot)){
+                if(checkDuration(timeslot)){
+                    if(checkWorkingDay(new DateTime(timeslot.getStartDate()), new DateTime(timeslot.getEndDate()))){
+
+                        return "You can reserve only one timeslot for current day!";
+                    }
+                    return "Working time isn't valid!";
+                }
+                return "Duration must be between 30 and 120 minutes!";
+            }
+            return  "Selected dates must be in future!";
+        }
+        return "Selected date must be at same day, and start has to be before end!";
+    }
+
+    public boolean checkDate(Timeslot timeslot){
+        if(timeslot.getStartDate().before(new Date()) || timeslot.getEndDate().before(new Date()) ){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkValidationDate(Timeslot timeslot){
+        if(timeslot.getStartDate().after(timeslot.getEndDate()) || !DateUtils.isSameDay(timeslot.getStartDate(), timeslot.getEndDate())){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkDuration(Timeslot timeslot){
+        Minutes minutes = Minutes.minutesBetween(new DateTime(timeslot.getStartDate()), new DateTime(timeslot.getEndDate()));
+        if(minutes.isGreaterThan(Minutes.minutes(120)) || minutes.isLessThan(Minutes.minutes(30))){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkWorkingDay(DateTime dateStart, DateTime dateEnd){
+        if(dateStart.dayOfWeek().get() == DateTimeConstants.SATURDAY || dateStart.dayOfWeek().get() == DateTimeConstants.SUNDAY){
+            if(dateStart.getHourOfDay() < 17 || dateEnd.getHourOfDay() > 22){
+                return false;
+            }
+        } else {
+            if(dateStart.getHourOfDay() < 18 || dateEnd.getHourOfDay() > 23){
+                return false;
+            }
+        }
+        return true;
     }
 }
