a2ec79cbc78ac09cce41c65d0b2c923fb28aec8e
Danilo Rapaic <rapaic.danilo9@gmail.com>
2022-10-20 09:39:38 +0200

    feat: Added Jacoco plugin

--- Tennis-scheduler/src/test/java/com/tennis/tennisscheduler/TennisSchedulerApplicationTests.java
+++ Tennis-scheduler/src/test/java/com/tennis/tennisscheduler/TennisSchedulerApplicationTests.java
@@ -3,7 +3,7 @@ package com.tennis.tennisscheduler;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TennisSchedulerApplicationTests {
 
 	@Test
